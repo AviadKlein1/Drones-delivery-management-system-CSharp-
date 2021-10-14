d5ec564ab47cebb0c1710faead2e9ead92cf1c9e
Aviad <Aviad.kle@gmail.com>
2021-10-14 14:04:46 +0300

    begin

---
+++ DAL/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace DAL
+{
+    namespace DO
+    {
+        public struct Station
+        {
+            public string Name { get; set; }
+        }
+
+        public struct Station
+        {
+            public string Name { get; set; }
+        }
+
+        public struct Station
+        {
+            public string Name { get; set; }
+        }
+
+        public struct Station
+        {
+            public string Name { get; set; }
+        }
+
+        public struct Station
+        {
+            public string Name { get; set; }
+        }
+
+        public struct Station
+        {
+            public string Name { get; set; }
+        }
+
+
+    }
+
+    namespace DalObject
+    { 
+    
+    }
+}
+
